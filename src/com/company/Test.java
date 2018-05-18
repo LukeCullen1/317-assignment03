74a4c7b3e9d612b29319009ea27236af856b7b34
Cullen, Luke William <lukewcullen@gmail.com>
2018-05-18 20:16:08 +1200

    double ended queue working with add to front, add to back, delete front and back and print front and back methods

--- src/com/company/Test.java
+++ src/com/company/Test.java
@@ -3,12 +3,26 @@ package com.company;
 public class Test {
      public static void main(String[] args) {
 
-         if (args.length != 1) {
+         /*if (args.length != 1) {
              System.out.println("err");
              return;
          }
          String p = args[0];
-         System.out.println(p);
+         System.out.println(p);*/
+
+         DEqueue d = new DEqueue();
+         for(int i = 0; i < 15; i++)
+         {
+             d.addToEnd(""+i);
+         }
+         for(int j = 100; j<120; j++)
+         {
+             d.addToFront(""+j);
+         }
+         d.removeEnd();
+         d.removeFront();
+         d.printHead();
+         d.printTail();
      }
 
 }
